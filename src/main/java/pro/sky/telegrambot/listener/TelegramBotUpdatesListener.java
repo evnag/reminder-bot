ed8ca04779e1fc2e49f2b7ddf7486b666173f1d2
evnag <vskonev22@gmail.com>
2022-12-05 12:38:04 +0300

    feat: add logic for responding to "/start" message

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,7 @@ import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
@@ -28,9 +30,25 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            String welcomeMessage = "Welcome to the reminder bot! Enter a description of the task in the format \"01.01.2022 20:00 Some text\"";
+            String messageText = update.message().text();
+            Long chatId = update.message().chat().id();
+
+            if (messageText.startsWith("/start")) {
+                logger.info("Message received: {}", messageText);
+
+                sendMessage(chatId, welcomeMessage);
+                logger.info("Message sent: {}", welcomeMessage);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+
+    private void sendMessage(Long chatId, String message) {
+        SendMessage messageToSent = new SendMessage(chatId, message);
+        SendResponse response = telegramBot.execute(messageToSent);
+        response.isOk();
+    }
+
 
 }
