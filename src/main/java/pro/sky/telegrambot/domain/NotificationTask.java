b0b3ee1e5a4f3f27770d3889225a5a0500a5238e
evnag <vskonev22@gmail.com>
2022-12-05 13:21:39 +0300

    feat: add @entity class for notification task

---
+++ src/main/java/pro/sky/telegrambot/domain/NotificationTask.java
@@ -0,0 +1,60 @@
+package pro.sky.telegrambot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class NotificationTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long chatId;
+    private String messageText;
+    private LocalDateTime dateTime;
+
+    public NotificationTask() {
+    }
+
+    public NotificationTask(Long chatId, String messageText, LocalDateTime dateTime) {
+        this.chatId = chatId;
+        this.messageText = messageText;
+        this.dateTime = dateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
