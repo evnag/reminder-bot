46ccdb2a2fbfc6a983986d07918f21166e0a1f0d
evnag <vskonev22@gmail.com>
2022-12-06 15:31:24 +0300

    refactor: added null check for update.message().text()

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -42,28 +42,34 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            String messageText = update.message().text();
-            Long chatId = update.message().chat().id();
 
-            if (messageText.startsWith(START_MESSAGE)) {
-                logger.info("Message received: {}", messageText);
-                sendMessage(chatId, WELCOME_MESSAGE);
-                logger.info("Message sent: {}", WELCOME_MESSAGE);
-            } else {
-                Matcher matcher = PATTERN.matcher(messageText);
-                if (matcher.matches()) {
-                    String date = matcher.group(1);
-                    messageText = messageText.replace(date, "").trim();
+            if (update.message().text() != null) {
+                String messageText = update.message().text();
+                Long chatId = update.message().chat().id();
 
-                    LocalDateTime localDateTime = LocalDateTime.parse(date, DATE_TIME_FORMATTER);
-                    createTask(chatId, messageText, localDateTime);
-                    sendMessage(chatId, SUCCESSFULLY_SAVED_MESSAGE);
-                    logger.info("Message sent: {}", SUCCESSFULLY_SAVED_MESSAGE);
-                    logger.info("Invoke method for creating task");
+                if (messageText.startsWith(START_MESSAGE)) {
+                    logger.info("Message received: {}", messageText);
+                    sendMessage(chatId, WELCOME_MESSAGE);
+                    logger.info("Message sent: {}", WELCOME_MESSAGE);
                 } else {
-                    sendMessage(chatId, WRONG_MESSAGE_FORMAT);
-                    logger.info("Message sent: {}", WRONG_MESSAGE_FORMAT);
+                    Matcher matcher = PATTERN.matcher(messageText);
+                    if (matcher.matches()) {
+                        String date = matcher.group(1);
+                        messageText = messageText.replace(date, "").trim();
+
+                        LocalDateTime localDateTime = LocalDateTime.parse(date, DATE_TIME_FORMATTER);
+                        createTask(chatId, messageText, localDateTime);
+                        sendMessage(chatId, SUCCESSFULLY_SAVED_MESSAGE);
+                        logger.info("Message sent: {}", SUCCESSFULLY_SAVED_MESSAGE);
+                        logger.info("Invoke method for creating task");
+                    } else {
+                        sendMessage(chatId, WRONG_MESSAGE_FORMAT);
+                        logger.info("Message sent: {}", WRONG_MESSAGE_FORMAT);
+                    }
                 }
+            } else {
+                sendMessage(update.message().chat().id(), UNKNOWN_COMMAND_FORMAT);
+                logger.info("Message sent: {}", UNKNOWN_COMMAND_FORMAT);
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
--- src/main/java/pro/sky/telegrambot/message/MessageConstants.java
+++ src/main/java/pro/sky/telegrambot/message/MessageConstants.java
@@ -6,4 +6,5 @@ public class MessageConstants {
     public final static String SUCCESSFULLY_SAVED_MESSAGE = "The event was successfully saved";
     public final static String WRONG_MESSAGE_FORMAT = "Wrong message format";
     public final static String WELCOME_MESSAGE = "Welcome to the reminder bot! Enter a description of the task in the format \"01.01.2022 20:00 Some text\"";
+    public final static String UNKNOWN_COMMAND_FORMAT = "Unknown command format. Try again!";
 }
